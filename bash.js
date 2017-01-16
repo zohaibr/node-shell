cce0feaa639ae4504f8d78da349c22dc19c6eb56
Zohaib Rahman <zohaibr@gmail.com>
2017-01-16 10:15:44 -0500

    Refactored Code

--- bash.js
+++ bash.js
@@ -4,12 +4,6 @@ command.prompt();
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remove the newline
-  if (cmd === 'pwd') {
-    command.pwd();
-  } else if (cmd === 'date') {
-    command.date();
-  } else {
-    command.prompt();
-  }
+  let cmd = data.toString().trim(); // remove the newline
+  command[cmd]();
 });
--- command.js
+++ command.js
@@ -1,14 +1,20 @@
-exports.pwd = function() {
-  process.stdout.write(process.cwd());
-  process.stdout.write('\nprompt > ');
+let commandManager = {
+
+  pwd: function() {
+    process.stdout.write(process.cwd());
+    process.stdout.write('\nprompt > ');
+  },
+
+  date: function() {
+    var date = new Date().toString().trim();
+    process.stdout.write(date);
+    process.stdout.write('\nprompt > ');
+  },
+
+ prompt: function() {
+    process.stdout.write('prompt > ');
+  }
+
 };
 
-exports.date = function() {
-  var date = new Date().toString().trim();
-  process.stdout.write(date);
-  process.stdout.write('\nprompt > ');
-};
-
-exports.prompt = function() {
-  process.stdout.write('prompt > ');
-};
+module.exports = commandManager;
